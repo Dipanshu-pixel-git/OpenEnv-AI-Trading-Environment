a6b0197117ac54bfc3dff94d1d827bc42489ab6d
Dipanshu Patle <patled854@gmail.com>
2026-04-12 23:06:50 +0530

    Add files via upload

--- inference.py
+++ inference.py
@@ -1,31 +1,33 @@
-from fastapi import FastAPI
 import numpy as np
 import pandas as pd
 from env.trading_env import TradingEnv
 
-app = FastAPI()
-
-# create dummy data
+# Create dummy data
 df = pd.DataFrame({
-    'Open': np.random.rand(100)*100,
-    'High': np.random.rand(100)*100,
-    'Low': np.random.rand(100)*100,
-    'Close': np.random.rand(100)*100,
-    'Volume': np.random.randint(100,1000,100)
+    'Open': np.random.rand(10)*100,
+    'High': np.random.rand(10)*100,
+    'Low': np.random.rand(10)*100,
+    'Close': np.random.rand(10)*100,
+    'Volume': np.random.randint(100,1000,10)
 })
 
 env = TradingEnv(df)
 
-@app.post("/reset")
-def reset():
-    state = env.reset()
-    return state.dict()
+print("[START]")
 
-@app.post("/step")
-def step(action: int):
+state = env.reset()
+print("Initial State:", state)
+
+done = False
+
+while not done:
+    action = np.random.choice([0,1,2])
     result = env.step(action)
-    return {
-        "state": result.state.dict(),
-        "reward": result.reward,
-        "done": result.done
-    }
+
+    print("[STEP]")
+    print("Action:", action)
+    print("Reward:", result.reward)
+
+    done = result.done
+
+print("[END]")
